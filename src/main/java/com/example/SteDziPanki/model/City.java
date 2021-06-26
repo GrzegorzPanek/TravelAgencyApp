8366a3c0b1cb3216fb052a3971ce927688c81acc
GrzegorzPanek <pgrzegorz93@gmail.com>
2021-06-26 15:07:13 +0200

    Stworzenie modelu

---
+++ src/main/java/com/example/SteDziPanki/model/City.java
@@ -0,0 +1,45 @@
+package com.example.SteDziPanki.model;
+
+import javax.persistence.*;
+
+@Entity
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public City(Integer id) {
+        this.id = id;
+    }
+
+    public City() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
