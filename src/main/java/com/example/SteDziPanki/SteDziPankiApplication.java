a4d730eee3f943fc58fb2a18d773a262953d3660
StecLukasz <lukaszstec14@gmail.com>
2021-06-26 11:43:59 +0200

    Initial commit SteDziPanki Rules

---
+++ src/main/java/com/example/SteDziPanki/SteDziPankiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SteDziPanki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteDziPankiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteDziPankiApplication.class, args);
+	}
+
+}
